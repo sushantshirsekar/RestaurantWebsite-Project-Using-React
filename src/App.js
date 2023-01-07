30239c695304723ffbb5e77702e34b9cbe535447
sushantshirsekar <sushantshirsekar60@gmail.com>
2023-01-07 12:16:55 +0530

    SummaryAdded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Layout/Header";
+import Summary from "./components/Meals/Summary";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Summary />
     </React.Fragment>
   );
 }
